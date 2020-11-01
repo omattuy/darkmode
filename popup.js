b5f8bef29fa17bc863ca1229b70227f8b56baabc
Otávio de Moraes Attuy <otavio.attuy@gmail.com>
2020-10-31 22:11:31 -0300

    Allowing the extesion to change background of all websites

--- popup.js
+++ popup.js
@@ -1,10 +1,10 @@
 let changeColor = document.getElementById('changeColor');
 let dark_mode = true;
 
-/* chrome.storage.sync.get('color', function(data) {
+chrome.storage.sync.get('color', function(data) {
   changeColor.style.backgroundColor = data.color;
   changeColor.setAttribute('value', data.color);
-}); */
+});
 
 changeColor.onclick = function(element) {
     let color = "white";

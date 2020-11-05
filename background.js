5a6f0882c59d92654586215102df3da94c330c62
Otávio de Moraes Attuy <otavio.attuy@gmail.com>
2020-11-05 03:45:28 -0300

    Styling changes

--- background.js
+++ background.js
@@ -1,7 +1,4 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#1b1d20'}, function() {
-        console.log("The color is #1b1d20.");
-    });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
           conditions: [new chrome.declarativeContent.PageStateMatcher({
--- popup.js
+++ popup.js
@@ -1,11 +1,6 @@
 let changeColor = document.getElementById('changeColor');
 let dark_mode = true;
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
-
 changeColor.onclick = function(element) {
     let color = "white";
     dark_mode = !dark_mode;
@@ -14,7 +9,6 @@ changeColor.onclick = function(element) {
     } else {
         color = "black";
     }
-    //let color = element.target.value;
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.executeScript(
           tabs[0].id,
